3c33871a3f939eb0c2f1997fdc4f79b8d357e78a
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-09-05 15:35:46 +0300

    css

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,14 @@
+import css from "./SearchBox.module.css";
 export default function SearchBox({ value, onFilter }) {
   return (
-    <>
-      <label htmlFor="username">Find contactc by name</label>
+    <div className={css.search}>
+      <label htmlFor="username">Find contacts by name</label>
       <input
         value={value}
         onChange={(evt) => onFilter(evt.target.value)}
         type="text"
         id="username"
       />
-    </>
+    </div>
   );
 }
